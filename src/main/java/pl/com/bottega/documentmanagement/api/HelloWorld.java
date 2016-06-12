11080fc3bd0c8a97bdb2c27c8508d171f48ab0a8
Maciej Zubala <maciej.zubala@gmail.com>
2016-06-12 11:34:55 +0200

    Added main method

--- src/main/java/pl/com/bottega/documentmanagement/api/HelloWorld.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/HelloWorld.java
@@ -4,4 +4,10 @@ package pl.com.bottega.documentmanagement.api;
  * Created by maciuch on 12.06.16.
  */
 public class HelloWorld {
+  
+  public static void main(String[] args) {
+    
+  }
+  
+  
 }
