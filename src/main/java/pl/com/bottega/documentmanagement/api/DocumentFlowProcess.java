51cc81c0d33a8518866d113d159b93d42b2e4d43
bartosz.paszkowski <bartula87@gmail.com>
2016-06-12 15:32:35 +0200

    API methods parameters validation

--- src/main/java/pl/com/bottega/documentmanagement/api/DocumentFlowProcess.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/DocumentFlowProcess.java
@@ -1,6 +1,10 @@
 package pl.com.bottega.documentmanagement.api;
 
+import com.google.common.base.Preconditions;
 import pl.com.bottega.documentmanagement.domain.DocumentNumber;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Created by bartosz.paszkowski on 12.06.2016.
@@ -9,26 +13,35 @@ public class DocumentFlowProcess {
 
 
     public DocumentNumber create(String title, String content) {
+        checkNotNull(title);
+        checkNotNull(content);
+
         return null;
     }
 
     public void change(DocumentNumber documentNumber,String newTitle, String newContent){
-
+        checkNotNull(documentNumber);
+        checkNotNull(newTitle);
+        checkNotNull(newContent);
     }
 
     public void verify(DocumentNumber documentNumber){
+        checkNotNull(documentNumber);
 
     }
 
     public void publish(DocumentNumber documentNumber) {
+        checkNotNull(documentNumber);
 
     }
 
     public void archive(DocumentNumber documentNumber){
+        checkNotNull(documentNumber);
 
     }
 
     public DocumentNumber createNewVersion(DocumentNumber documentNumber){
+        checkNotNull(documentNumber);
         return null;
     }
 
