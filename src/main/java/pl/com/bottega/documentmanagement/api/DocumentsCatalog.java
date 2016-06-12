0492cbed1cf5537530bdecc6df3724ab9189d83f
anna <krzysztoszek.a@gmail.com>
2016-06-12 15:22:28 +0200

    Api methods

---
+++ src/main/java/pl/com/bottega/documentmanagement/api/DocumentsCatalog.java
@@ -0,0 +1,18 @@
+package pl.com.bottega.documentmanagement.api;
+
+import pl.com.bottega.documentmanagement.domain.Document;
+import pl.com.bottega.documentmanagement.domain.DocumentNumber;
+
+/**
+ * Created by anna on 12.06.2016.
+ */
+public class DocumentsCatalog {
+
+    public Document get(DocumentNumber documentNumber) {
+        return null;
+    }
+
+    public Iterable<Document> find(DocumentCriteria documentCriteria) {
+        return null;
+    }
+}
---
+++ src/main/java/pl/com/bottega/documentmanagement/domain/Employee.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.documentmanagement.domain;
+
+/**
+ * Created by anna on 12.06.2016.
+ */
+public class Employee {
+}
