eeda5cc47f44f535fdaece9deb28c9518bf42555
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-07-06 21:04:14 +0200

    DocumentStatus added Employee verificator added

--- src/main/java/pl/com/bottega/documentmanagement/domain/Document.java
+++ src/main/java/pl/com/bottega/documentmanagement/domain/Document.java
@@ -12,30 +12,45 @@ public class Document {
     DocumentNumber documentNumber;
     String content;
     String title;
+    DocumentStatus documentStatus;
     @Id
     @GeneratedValue
     Long id;
     @ManyToOne
     private Employee creator;
+    @ManyToOne
+    private Employee verificator;
 
-    public Document(DocumentNumber documentNumber, String content, String title, Employee creator) {
+
+    public Document(DocumentNumber documentNumber, String content, String title, Employee creator, DocumentStatus documentStatus, Employee verificator) {
         this.documentNumber = documentNumber;
         this.content = content;
         this.title = title;
         this.creator = creator;
+        this.documentStatus = documentStatus;
+        this.verificator = verificator;
     }
+
     private Document(){}
+
+    @Enumerated(EnumType.STRING)
+    public DocumentStatus getDocumentStatus() {
+        return documentStatus;
+    }
 
     public void change(String title, String content) {
         this.title = title;
         this.content = content;
+        documentStatus = documentStatus.DRAFT;
     }
 
     public void verify(Employee employee) {
+        this.verificator = employee;
+        documentStatus = documentStatus.VERIFIED;
 
     }
 
-    public void confirm(Employee conirmator) {
+    public void confirm(Employee confirmator) {
 
     }
 
---
+++ src/main/java/pl/com/bottega/documentmanagement/domain/DocumentStatus.java
@@ -0,0 +1,9 @@
+package pl.com.bottega.documentmanagement.domain;
+
+/**
+ * Created by paulina.pislewicz on 2016-07-05.
+ */
+public enum DocumentStatus {
+    DRAFT,
+    VERIFIED
+}
