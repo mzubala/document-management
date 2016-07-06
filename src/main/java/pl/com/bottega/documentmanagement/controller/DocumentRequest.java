21220fbdd4724f79708ab08aadb20706092262a7
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-07-06 21:02:10 +0200

    DocumentNumber added

--- src/main/java/pl/com/bottega/documentmanagement/controller/DocumentRequest.java
+++ src/main/java/pl/com/bottega/documentmanagement/controller/DocumentRequest.java
@@ -1,11 +1,14 @@
 package pl.com.bottega.documentmanagement.controller;
 
+import pl.com.bottega.documentmanagement.domain.DocumentNumber;
+
 /**
  * Created by paulina.pislewicz on 2016-07-03.
  */
 public class DocumentRequest {
     private String title;
     private String content;
+    private DocumentNumber documentNumber;
 
     public String getTitle() {
         return title;
@@ -21,5 +24,13 @@ public class DocumentRequest {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public DocumentNumber getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(DocumentNumber documentNumber) {
+        this.documentNumber = documentNumber;
     }
 }
