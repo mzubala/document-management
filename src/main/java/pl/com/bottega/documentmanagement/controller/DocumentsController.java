c2ea3c57947a5aa05b778cddd89beeae9f847c6d
Maciej Zubala <maciej.zubala@gmail.com>
2016-07-10 16:57:02 +0200

    Get doocuments

--- src/main/java/pl/com/bottega/documentmanagement/controller/DocumentsController.java
+++ src/main/java/pl/com/bottega/documentmanagement/controller/DocumentsController.java
@@ -1,6 +1,7 @@
 package pl.com.bottega.documentmanagement.controller;
 
 import org.springframework.web.bind.annotation.*;
+import pl.com.bottega.documentmanagement.api.DocumentCriteria;
 import pl.com.bottega.documentmanagement.api.DocumentDto;
 import pl.com.bottega.documentmanagement.api.DocumentFlowProcess;
 import pl.com.bottega.documentmanagement.api.DocumentsCatalog;
@@ -34,6 +35,12 @@ public class DocumentsController {
     @GetMapping("/{documentNumber}")
     public DocumentDto show(@PathVariable String documentNumber) {
         return documentsCatalog.get(new DocumentNumber(documentNumber));
+    }
+
+    // /documents?query=hospital&createdBy=40&verifiedBy=76&...&...
+    @GetMapping
+    public Iterable<DocumentDto> index(DocumentCriteria documentCriteria) {
+        return documentsCatalog.find(documentCriteria);
     }
 
 }
