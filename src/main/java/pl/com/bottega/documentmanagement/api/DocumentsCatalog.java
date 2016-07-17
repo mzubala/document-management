f2f94143a6c9f6d18b59808df1497805d66bf764
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-07-17 07:59:38 +0200

    change returned object from List to Iterable

--- src/main/java/pl/com/bottega/documentmanagement/api/DocumentsCatalog.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/DocumentsCatalog.java
@@ -2,13 +2,11 @@ package pl.com.bottega.documentmanagement.api;
 
 import pl.com.bottega.documentmanagement.domain.DocumentNumber;
 
-import java.util.List;
-
 /**
  * Created by paulina.pislewicz on 2016-07-10.
  */
 public interface DocumentsCatalog {
-    pl.com.bottega.documentmanagement.infrastructure.DocumentDto get(DocumentNumber documentNumber);
+    pl.com.bottega.documentmanagement.api.DocumentDto get(DocumentNumber documentNumber);
 
-    List<pl.com.bottega.documentmanagement.infrastructure.DocumentDto> find(DocumentCriteria documentCriteria);
+    Iterable <pl.com.bottega.documentmanagement.api.DocumentDto> find(DocumentCriteria documentCriteria);
 }
