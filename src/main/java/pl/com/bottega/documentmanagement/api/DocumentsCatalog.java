8f9286de46150c90c0304fe762ce5914e80d3b74
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-08-17 11:31:49 +0200

    created interface with method "find" according to documentCriteria

--- src/main/java/pl/com/bottega/documentmanagement/api/DocumentsCatalog.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/DocumentsCatalog.java
@@ -6,7 +6,7 @@ import pl.com.bottega.documentmanagement.domain.DocumentNumber;
  * Created by paulina.pislewicz on 2016-07-10.
  */
 public interface DocumentsCatalog {
-    pl.com.bottega.documentmanagement.api.DocumentDto get(DocumentNumber documentNumber);
+    DocumentDto get(DocumentNumber documentNumber);
 
-    Iterable <pl.com.bottega.documentmanagement.api.DocumentDto> find(DocumentCriteria documentCriteria);
+    DocumentSearchResults find(DocumentCriteria documentCriteria);
 }
