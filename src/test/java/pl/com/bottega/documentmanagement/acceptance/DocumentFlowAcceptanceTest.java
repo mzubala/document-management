5a94ce00669a02116ea23b14c320554b7f492bbf
SebastianBodzak <sebastianbodzak@yahoo.com>
2016-08-26 17:50:09 +0200

    acceptance tests part2 (cdn)

--- src/test/java/pl/com/bottega/documentmanagement/acceptance/DocumentFlowAcceptanceTest.java
+++ src/test/java/pl/com/bottega/documentmanagement/acceptance/DocumentFlowAcceptanceTest.java
@@ -11,15 +11,18 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
-import pl.com.bottega.documentmanagement.api.DocumentDto;
-import pl.com.bottega.documentmanagement.api.DocumentFlowProcess;
-import pl.com.bottega.documentmanagement.api.DocumentsCatalog;
-import pl.com.bottega.documentmanagement.api.UserManager;
+import pl.com.bottega.documentmanagement.api.*;
 import pl.com.bottega.documentmanagement.domain.DocumentNumber;
-import pl.com.bottega.documentmanagement.domain.DocumentStatus;
 import pl.com.bottega.documentmanagement.domain.EmployeeId;
 
+import javax.persistence.NoResultException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
+import static pl.com.bottega.documentmanagement.domain.DocumentStatus.PUBLISHED;
+import static pl.com.bottega.documentmanagement.domain.DocumentStatus.VERIFIED;
 
 /**
  * Created by Dell on 2016-08-20.
@@ -39,11 +42,15 @@ public class DocumentFlowAcceptanceTest {
     @Autowired
     private DocumentsCatalog documentsCatalog;
 
+    @Autowired
+    private ReadingConfirmator readingConfirmator;
+
     private String anyLogin = "xxx";
     private String anyPassword = "yyy";
     private EmployeeId anyEmployeeId = new EmployeeId(1L);
     private String anyTitle = "title";
     private String anyContent = "content";
+    private Set<EmployeeId> readers = new HashSet<>(Arrays.asList(anyEmployeeId, new EmployeeId(10L), new EmployeeId(11L)));
 
 
     @Before
@@ -67,13 +74,73 @@ public class DocumentFlowAcceptanceTest {
     @Test
     @Transactional
     public void shouldVerifyDocument() {
-        loginAs("STAFF", "EDITOR", "MANAGER");
-        DocumentNumber nr = documentFlowProcess.create(anyTitle, anyContent);
+        DocumentNumber nr = createDocument();
 
         documentFlowProcess.verify(nr);
 
         DocumentDto dto = documentsCatalog.get(nr);
-        assertEquals(DocumentStatus.VERIFIED.toString(), dto.getStatus());
+        assertEquals(VERIFIED.toString(), dto.getStatus());
+    }
+
+    @Test
+    @Transactional
+    public void shouldPublishDocument() {
+        DocumentNumber nr = createDocument();
+        //todo mock method sending emails
+        //todo mock method printing documents
+
+        documentFlowProcess.publish(nr, readers);
+
+        DocumentDto dto = documentsCatalog.get(nr);
+        assertEquals(PUBLISHED.toString(), dto.getStatus());
+    }
+
+    @Test(expected = NoResultException.class)
+    @Transactional
+    public void shouldNotGetArchivedDocument() {
+        DocumentNumber nr = createDocument();
+        documentFlowProcess.archive(nr);
+
+        DocumentDto dto = documentsCatalog.get(nr);
+    }
+
+    @Test
+    @Transactional
+    public void shouldConfirmDocumentHasBeenRead() {
+        DocumentNumber nr = createDocument();
+        documentFlowProcess.publish(nr, readers);
+
+        readingConfirmator.confirm(nr);
+
+//        Reader reader = ; // TODO: 2016-08-26
+    }
+
+    @Test
+    @Transactional
+    public void shouldConfirmDocumentByOtherEmployee() {
+        DocumentNumber nr = createDocument();
+        documentFlowProcess.publish(nr, readers);
+
+        readingConfirmator.confirm(nr, new EmployeeId(10L));
+
+//        Reader reader = ; // TODO: 2016-08-26
+    }
+
+    @Test
+    public void shouldSignUp() {
+        SignupResultDto result = userManager.signup(anyLogin, anyPassword, anyEmployeeId);
+
+        assertTrue(result.isSuccess());
+        SignupResultDto shouldLogin = userManager.login(anyLogin, anyPassword);
+        assertTrue(shouldLogin.isSuccess());
+
+    }
+
+    private DocumentNumber createDocument() {
+        loginAs("STAFF", "EDITOR", "MANAGER");
+        //todo mock method sending emails
+        //todo mock method printing documents
+        return documentFlowProcess.create(anyTitle, anyContent);
     }
 
     private void loginAs(String... roles) {
